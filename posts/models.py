384edade0c483e525f5f2ddd9f0a46c79d044f4d
Witcape <bshaurya.me@gmail.com>
2025-06-06 20:44:50 +0530

    Completed Project

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class POST(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=1000000)
+    created_at = models.DateTimeField(datetime.now, blank=True)
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import POST
 # Create your views here.
 
 def index(response):
-    return render(response, "index.html")
+    posts = POST.objects.all()
+    return render(response, "index.html", {'posts': posts})
+
+def post(response, pk):
+    post = POST.objects.get(id = pk)
+    return render(response, "posts.html", {'post': post})
